db44125224a65c17e6ac0e2e5d9beca8d8f143ad
Kenichi Matsubara <wakashijp@gmail.com>
2018-01-04 12:55:08 +0900

    Initial commit

---
+++ Chapter05/Section5-1/if_and.py
@@ -0,0 +1,13 @@
+# randomモジュールのrandint関数を読み込む
+from random import randint
+size = randint(5, 20)  # 5〜20の乱数を生成する。
+weight = randint(20, 40)  # 20〜40の乱数を生成する。
+# 判定（両方の条件がTrueの時合格）
+if (size >= 10) and (weight >= 25):
+    result = "合格"
+else:
+    result = "不合格"
+
+# 結果の出力
+text = f"サイズ{size}、重量{weight}：{result}"
+print(text)
