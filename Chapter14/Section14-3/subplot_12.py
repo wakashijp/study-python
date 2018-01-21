3d17c2c8f8d3c55607a6afa3420f77514f6bfb66
Kenichi Matsubara <wakashijp@gmail.com>
2018-01-22 07:56:20 +0900

    Initial commit

---
+++ Chapter14/Section14-3/subplot_12.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+X1, Y1 = range(0, 5), [61, 45, 27, 88, 47]
+X2, Y2 = range(0, 5), [17, 39, 46, 40, 27]
+labels = ["A", "B", "C", "D", "E"]
+fig = plt.figure()  # 図を作る
+
+# 1行2列の左
+ax1 = fig.add_subplot(1, 2, 1)  # サブプロットを追加する
+ax1.bar(X1, Y1, color="b", tick_label=labels)  # グラフの描画
+ax1.set_title("dog")  # グラフのタイトル
+
+# 1行2列の右
+ax2 = fig.add_subplot(1, 2, 2)  # サブプロットを追加する
+ax2.bar(X2, Y2, color="g", tick_label=labels)  # グラフの描画
+ax2.set_title("cat")  # グラフのタイトル
+plt.show()
