462db45b9ca10f59b2f78c1554828572e6503dab
Kenichi Matsubara <wakashijp@gmail.com>
2018-01-21 22:24:12 +0900

    Initial commit

---
+++ Chapter14/Section14-2/pyplot_scatter_marker2.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+X, Y = np.random.rand(100), np.random.rand(100)  # ランダムな配列を作る
+plt.scatter(X, Y, marker="o", s=500, color="cyan", alpha=0.5,
+            linewidths=2, edgecolors="b")  # グラフを描く
+plt.show()
